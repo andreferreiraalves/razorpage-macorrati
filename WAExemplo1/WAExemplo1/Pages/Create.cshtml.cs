eb29874a5eb0fb34697a44fe804895ee5673cbda
Andre Ferreira <andre.ferreira@mercatustecnologia.com.br>
2020-07-22 20:08:32 -0300

    add do projeto

---
+++ WAExemplo1/WAExemplo1/Pages/Create.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+using WAExemplo1.Models;
+
+namespace WAExemplo1.Pages
+{
+    public class CreateModel : PageModel
+    {
+        private readonly AppDbContext _context;
+
+        [BindProperty]
+        public Cliente Cliente { get; set; }
+
+        public CreateModel(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            _context.Clientes.Add(Cliente);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("/Index");
+        }
+    }
+}
---
+++ WAExemplo1/WAExemplo1/Pages/Index.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using WAExemplo1.Models;
+
+namespace WAExemplo1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly AppDbContext _context;
+        public IList<Cliente> Clientes { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger, AppDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+
+        public async Task OnGetAsync()
+        {
+            Clientes = await _context.Clientes.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(int id)
+        {
+            var contact = await _context.Clientes.FindAsync(id);
+            if(contact != null)
+            {
+                _context.Clientes.Remove(contact);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage();
+        }
+
+        public IActionResult OnPostTeste(Cliente c)
+        {
+            Console.WriteLine(c.Nome);
+            return RedirectToPage();
+        }
+    }
+}
