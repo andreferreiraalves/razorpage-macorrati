f5b6d8c8c5b277bc05dbc21054bed1385b41c172
Andre Ferreira <andre.ferreira@mercatustecnologia.com.br>
2020-07-22 20:20:52 -0300

    término do exemplo

---
+++ WAExemplo1/WAExemplo1/Pages/Edit.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WAExemplo1.Models;
+
+namespace WAExemplo1.Pages
+{
+    public class EditModel : PageModel
+    {
+        private readonly AppDbContext _context;
+
+        [BindProperty]
+        public Cliente Cliente { get; set; }
+
+        public EditModel(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Cliente = await _context.Clientes.FindAsync(id);
+
+            if (Cliente == null)
+                return RedirectToPage("/index");
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            _context.Attach(Cliente).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw new Exception($"Cliente {Cliente.Id} não encontrado");
+            }
+            return RedirectToPage("./index");
+        }
+    }
+}
